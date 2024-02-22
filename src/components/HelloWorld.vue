9adc8b32be8b49c9e8fb81bf4b71dfa89e2c76b4
Andrey Khachatryan <andrey@omg.lol>
2024-02-22 14:31:02 +0300

    add button event listener

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,13 +6,25 @@ defineProps({
 })
 
 const count = ref(0)
+
+let tg = window.Telegram.WebApp
+function listener() {
+  count.value++;
+  if (!tg.MainButton.isVisible) {
+    tg.MainButton.show();
+  }
+
+  tg.MainButton.setText(count.value);
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="listener()">
+      count is {{ count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
