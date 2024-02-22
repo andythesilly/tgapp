aac434678fae770434fadd4c7f614cd259c8c1fd
Andrey Khachatryan <andrey@omg.lol>
2024-02-23 02:37:12 +0300

    add + - buttons & add hint text style

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import { ref } from 'vue'
 const cart = ref([])
 const total = ref(0)
 const items = ref([
-  { emoji: '🍔', title: 'Бургер', price: 150 },
-  { emoji: '🍟', title: 'Фри', price: 100 },
-  { emoji: '🍕', title: 'Пицца', price: 300 },
+  { emoji: '🍔', title: 'Бургер', price: 150, isAdding: true, quantity: 0 },
+  { emoji: '🍟', title: 'Фри', price: 100, isAdding: true, quantity: 0 },
+  { emoji: '🍕', title: 'Пицца', price: 300, isAdding: true, quantity: 0 },
 ])
 
 const tg = window.Telegram.WebApp
@@ -16,9 +16,38 @@ function addToCart(item) {
   }
 
   cart.value.push(item)
-  total.value += item.price
 
+  item.isAdding = false
+  item.quantity = 1
+
+  total.value += item.price
   tg.MainButton.setText(total.value)
+}
+
+function increaseToCart(item) {
+  item.quantity++
+
+  total.value += item.price
+  tg.MainButton.setText(total.value)
+}
+
+function decreaseFromCart(item) {
+  item.quantity--
+  total.value -= item.price
+  tg.MainButton.setText(total.value)
+
+  if (item.quantity === 0) {
+    const index = cart.value.indexOf(item)
+    if (index != -1) {
+      cart.value.splice(index, 1)
+    }
+
+    item.isAdding = true
+  }
+
+  if (total.value == 0) {
+    tg.MainButton.hide()
+  }
 }
 </script>
 
@@ -31,12 +60,24 @@ function addToCart(item) {
       <div class="text-7xl">{{ item.emoji }}</div>
       <div class="flex flex-row py-2">
         <div>{{ item.title }}</div>
-        <div class="mx-1">·</div>
+        <div class="mx-1 hint">·</div>
         <div class="font-bold">{{ item.price }}</div>
       </div>
-      <div>
+      <div
+        v-if="item.isAdding"
+        class="flex flex-row justify-center items-center"
+      >
         <button class="rounded-full py-1 px-3" @click="addToCart(item)">
           Добавить
+        </button>
+      </div>
+      <div v-else class="flex flex-row justify-center items-center">
+        <button class="rounded-full py-1 px-3" @click="increaseToCart(item)">
+          +
+        </button>
+        <div class="hint">{{ item.quantity }}</div>
+        <button class="rounded-full py-1 px-3" @click="decreaseFromCart(item)">
+          -
         </button>
       </div>
     </div>
