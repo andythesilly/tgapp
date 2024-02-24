56efd434120aadef1e531b59a109355a53c70414
Andrey Khachatryan <andrey@omg.lol>
2024-02-24 13:43:05 +0300

    hotfix styles

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -13,33 +13,36 @@ defineEmits(['add-to-cart', 'increase-to-cart', 'decrease-from-cart'])
 <template>
   <div
     v-show="!item.isAdding"
-    class="counter right-6 top-1 rounded-full px-2 font-bold"
+    class="counter right-1 top-1 rounded-full px-2 font-bold"
   >
     {{ quantity }}
   </div>
   <div class="text-7xl">{{ item.emoji }}</div>
-  <div class="flex flex-row py-2">
-    <div>{{ item.title }}</div>
+  <div class="flex flex-row items-center py-2">
+    <div class="text-base">{{ item.title }}</div>
     <div class="mx-1 hint">·</div>
-    <div class="font-bold">{{ item.price }}</div>
+    <div class="font-bold text-sm">{{ item.price }}</div>
   </div>
   <div
     v-if="item.isAdding"
     class="flex flex-row justify-center items-center w-[60%]"
   >
-    <button class="rounded-full py-2 px-3" @click="$emit('add-to-cart')">
+    <button
+      class="rounded-full py-2 px-3 font-bold"
+      @click="$emit('add-to-cart')"
+    >
       Добавить
     </button>
   </div>
   <div v-else class="flex flex-row justify-center items-center w-full">
     <button
-      class="rounded-full p-2 w-[30%] font-bold mr-1"
+      class="rounded-full p-2 w-[50%] font-bold mr-1"
       @click="$emit('increase-to-cart')"
     >
       +
     </button>
     <button
-      class="rounded-full p-2 w-[30%] font-bold ml-1"
+      class="rounded-full p-2 w-[50%] font-bold ml-1"
       @click="$emit('decrease-from-cart')"
     >
       -
